97513a37b33736014e3566fe59f64fc8518ca203
Oleg Poloviy <125209908+OlegPoloviy@users.noreply.github.com>
2024-12-17 00:31:39 +0200

    added user account page

--- StremingApp/frontend/StreamingApp/src/Pages/user/MainPage.jsx
+++ StremingApp/frontend/StreamingApp/src/Pages/user/MainPage.jsx
@@ -1,4 +1,5 @@
 import {useSelector} from "react-redux";
+import "../../Styles/userAccount.scss"
 
 export function MainPage() {
     const user = useSelector(store => store)
@@ -7,7 +8,15 @@ export function MainPage() {
         <>
             {
                 user.user && (
-                    <h2>Welcome, {user.user.login}</h2>
+                    <div className={'profile'}>
+                        <div className={'profile_container'}>
+                            <img src={user.user.image} alt=""/>
+                            <h2>{user.user.login}</h2>
+                            <p>{user.user.email}</p>
+                        </div>
+
+                    </div>
+
                 )
             }
         </>
